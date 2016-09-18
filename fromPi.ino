e19d3dfed65ef0f936fc14cd1c6cc28450682793
SikkandarSulaiman <sikkandarsulaiman@gmail.com>
2016-09-18 15:04:30 +0530

    Add files via upload

---
+++ fromPi.ino
@@ -0,0 +1,25 @@
+void setup() {
+  Serial.begin(115200);
+  pinMode(2,1);
+  pinMode(3,1);
+  pinMode(4,1);
+  pinMode(A3,1);
+  pinMode(A1,0);
+  analogWrite(A3,1024);
+}
+
+void loop() {
+  if(Serial.available()){
+    String recv = Serial.readStringUntil('\n');
+    if(recv.substring(0,2)=="on")
+      digitalWrite((recv.substring(2,4)).toInt(),1);
+    else if(recv.substring(0,3)=="off")
+      digitalWrite((recv.substring(3,5)).toInt(),0);
+    else if(recv.substring(0,3)=="tog")
+      digitalWrite((recv.substring(3,5)).toInt(),digitalRead((recv.substring(3,5)).toInt())?0:1);
+    else if(recv.substring(0,3)=="pwm")
+      analogWrite((recv.substring(3,5)).toInt(),(recv.substring(5,8)).toInt());
+    delay(200);
+    Serial.println(analogRead(A1));
+    }
+}
